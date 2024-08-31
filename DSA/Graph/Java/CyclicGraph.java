fa3eff730e8bf5498dcf0e10dd2d4b6fb9bc6dda
phoenix.mp3 <hemanthkarthick03@gmail.com>
2024-08-31 18:30:13 +0530

    Graph-2

--- DSA/Graph/Java/CyclicGraph.java
+++ DSA/Graph/Java/CyclicGraph.java
@@ -1,19 +1,20 @@
 import java.util.*;
+import java.io.*;
 
-class CyclicGraph{
+class CyclicGraph {
 
-    public static boolean cycleDFS(List<List<Integer>> graph, int x, boolean[] visit, boolean[] recStack){
+    public static boolean cycleDFS(List<List<Integer>> graph, int x, boolean[] visit, boolean[] recStack) {
         if (recStack[x])
             return true;
-        
+
         if (visit[x])
-            return true;
+            return false;
 
         visit[x] = true;
         recStack[x] = true;
 
-        for (int i : graph.get(x)){
-            if (cycleDFS(graph, i, visit, recStack)){
+        for (int i : graph.get(x)) {
+            if (cycleDFS(graph, i, visit, recStack)) {
                 return true;
             }
         }
@@ -24,25 +25,24 @@ class CyclicGraph{
 
     }
 
-    public static boolean isCyclic(List<List<Integer>> graph){
+    public static boolean isCyclic(List<List<Integer>> graph) {
         int n = graph.size();
-        boolean[] visit = new boolean[n+1];
-        boolean[] recStack = new boolean[n+1];
-        
-        for (int i=0; i<=n; i++){
-            visit[i] = true;
+        boolean[] visit = new boolean[n + 1];
+        boolean[] recStack = new boolean[n + 1];
+
+        for (int i = 0; i < n; i++) {
             if (cycleDFS(graph, i, visit, recStack))
                 return true;
         }
         return false;
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         List<List<Integer>> graph = new ArrayList<>();
-        
+
         int n = 5;
 
-        for (int i=0; i<=n; i++){
+        for (int i = 0; i < n; i++) {
             graph.add(new ArrayList<>());
         }
 
@@ -50,7 +50,7 @@ class CyclicGraph{
         graph.get(1).add(3);
         graph.get(2).add(3);
         graph.get(3).add(4);
-        graph.get(4).add(2);
+        graph.get(4).add(0);
 
         String s = (isCyclic(graph)) ? "Cyclic" : "Not Cyclic Graph";
 
